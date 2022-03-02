5f99003aaf7e6cc9dce4065cc1f1c2f41e70d13e
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-02 20:27:04 +0530

    Added: Welcome message

---
+++ src/com/bridgelabz/MyHashMapMain.java
@@ -0,0 +1,8 @@
+package com.bridgelabz;
+
+public class MyHashMapMain {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to hash table problems using Java Generics");
+	}
+}
