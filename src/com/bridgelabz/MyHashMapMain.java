f2ce31b77bf69cd83c92a188be0097525a7a5d92
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-03-17 21:14:05 +0530

    Added: Ability to find frequency of words in a large paragraph logic

--- src/com/bridgelabz/MyHashMapMain.java
+++ src/com/bridgelabz/MyHashMapMain.java
@@ -1,5 +1,7 @@
 /*
- *Ability to find frequency of words in a sentence like “To be or not to be”
+ *Ability to find frequency of words in a large paragraph phrase 
+ *“Paranoids are not paranoid because they are paranoid but because they keep putting themselves 
+ *deliberately into paranoid avoidable situations”
  */
 package com.bridgelabz;
 
@@ -7,6 +9,7 @@ public class MyHashMapMain {
 
 	public static void main(String[] args) {
 		givenSentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency();
+		givenSentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency();
 	}
 
 	public static void givenSentence_WhenWordsAreAddedToList_ShouldReturnWordFrequency() {
@@ -23,6 +26,22 @@ public class MyHashMapMain {
 		}
 		System.out.println(myHashMap);
 		int frequency = myHashMap.get("to");
+		System.out.println("frequency = " + frequency);
+	}
+
+	public static void givenSentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency() {
+		String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+		MyLinkedHashMap<String, Integer> myLinkedHashMap = new MyLinkedHashMap<>();
+		String[] words = sentence.toLowerCase().split(" ");
+		for (String word : words) {
+			Integer value = myLinkedHashMap.get(word);
+			if (value == null)
+				value = 1;
+			else
+				value += 1;
+			myLinkedHashMap.add(word, value);
+		}
+		int frequency = myLinkedHashMap.get("paranoid");
 		System.out.println("frequency = " + frequency);
 	}
 }
